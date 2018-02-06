3f79dfc0bcf713db6e9ee3a3d16102e7c6ac8def
James Daley <jamesdaley2011@gmail.com>
2018-02-05 23:34:08 -0800

    Adding items

--- js/src/todoList.js
+++ js/src/todoList.js
@@ -1,6 +1,49 @@
 var m = require('mithril');
 
-var list = [];
+var state = {
+    username: "admin",
+    value: "",
+    setValue: function(v){
+        state.value = v;
+    },
+    list: [],
+    listItems: []
+}
+
+function submit(e){
+    m.request({
+      method: "PUT",
+      url: 'api/todoList',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      data: {
+          user: {
+              username: state.username
+          },
+          name: state.value
+      }
+    }).then(function(result){
+      state.listItems.push({name: state.value});
+      state.value = "";
+    });
+}
+
+var InputTodo = {
+    view: function(vnode){
+        return m('div', [
+            m('input', {
+                type: 'text',
+                oninput: m.withAttr("value", state.setValue),
+                value: state.value,
+            }),
+            m('button', {
+                type: 'submit',
+                onclick: submit
+            }, 'Add')
+        ]);
+    }
+}
 
 module.exports = {
   oninit: function(vnode){
@@ -10,12 +53,14 @@ module.exports = {
       method: "GET",
       url: `api/user/${username}/todoLists`
     }).then(function(result){
-      list = result.map(item => m('li', item.name));
-    }).catch(function(error){
-      console.log(error);
+      state.listItems = result;
     });
   },
-  view: function(){
-    return m('ol', list)
+  view: function(vnode){
+    state.list = state.listItems.map(item => m('li', item.name));
+    return m('div', [
+        m(InputTodo),
+        m('ol', state.list)
+    ])
   }
 }
